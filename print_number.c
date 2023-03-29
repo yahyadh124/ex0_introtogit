5f2848fec531491b6be5247057d0bbf1a6812646
Yahya Dahamshe <d.yahya@campus.technion.ac.il>
2023-03-29 12:23:55 +0300

    prints 234124

--- print_number.c
+++ print_number.c
@@ -2,9 +2,10 @@
 
 int course_number[6] = {4, 2, 1, 4, 3, 2};
 int main() {
-	for(int i = 6; i > 0; i--) {
+	for(int i = 5; i >= 0; i--) {
 		printf("%d", course_number[i]);
 	}
 	printf("\n");
 	return 0;
 }
+
